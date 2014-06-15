b33967aade055d10c0c188a8739f1b933b866518
Luke Keller <lukeekeller@gmail.com>
2014-06-14 21:42:04 -0400

    invite builder tests

--- app/mailers/invite_mailer.rb
+++ app/mailers/invite_mailer.rb
@@ -3,7 +3,7 @@ class InviteMailer < ActionMailer::Base
 
   def send_invite(invite)
     @invite = invite
-    @organization = Organization.find(@invite[:organization_id].to_i)
+    @organization = Organization.find(@invite[:organization_id])
     mail to: @invite[:email], subject: "You're Invited to Join Hello Compost"
   end
 end
